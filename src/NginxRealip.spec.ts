0fbbee55a89eadb5a731e8d687d4bc13c8e7a831
Jørgen Vatle <Jorgen@WcAServices.net>
2019-02-25 06:01:48 +0200

    Assert that NginxRealip can build configuration files.

---
+++ src/NginxRealip.spec.ts
@@ -0,0 +1,11 @@
+import NginxRealip from "./NginxRealip";
+import expect = require("expect");
+
+describe('nginx realip', () => {
+    const nginxRealip = new NginxRealip();
+
+    it('can build a configuration file', async () => {
+        const config = await nginxRealip.buildConfig();
+        expect(config.length).toBeGreaterThan(0);
+    });
+});
