5c2236f64ebe8d257aa4b918c1cda43c29b0e235
Jørgen Vatle <Jorgen@WcAServices.net>
2019-02-25 04:23:50 +0200

    Update `cloudflare ips` count assertion to avoid failing for single ip counts.

--- src/CloudFlareIps.spec.ts
+++ src/CloudFlareIps.spec.ts
@@ -6,21 +6,21 @@ describe('cloudflare ips', () => {
 
     it('can fetch ipv4 plaintext lists', async () => {
         const ipv4List = await ips.getList(4);
-        expect(ipv4List.length).toBeGreaterThan(1);
+        expect(ipv4List.length).toBeGreaterThan(0);
     });
 
     it('can fetch ipv6 plaintext lists', async () => {
         const ipv6List = await ips.getList(6);
-        expect(ipv6List.length).toBeGreaterThan(1);
+        expect(ipv6List.length).toBeGreaterThan(0);
     });
 
     it('can fetch ipv4 arrays', async () => {
         const ipv4Array = await ips.getArray(4);
-        expect(ipv4Array.length).toBeGreaterThan(1);
+        expect(ipv4Array.length).toBeGreaterThan(0);
     });
 
     it('can fetch ipv6 arrays', async () => {
         const ipv6Array = await ips.getArray(6);
-        expect(ipv6Array.length).toBeGreaterThan(1);
+        expect(ipv6Array.length).toBeGreaterThan(0);
     });
 });
