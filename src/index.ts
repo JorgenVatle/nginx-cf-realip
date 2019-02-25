24806fbb249698fc8c2080bba2c001bb29382ec6
Jørgen Vatle <Jorgen@WcAServices.net>
2019-02-25 06:38:15 +0200

    Set defaults for CLI.

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
 import Commander from 'commander';
 
 Commander.version(require('../package.json').version)
-    .option('-d, --destination <path>', 'Destination path for NGINX realip list. [Default: /etc/nginx/util]')
-    .option('-h, --h <header>', 'Header to fetch realip from. defaults to [Default: CF-Connecting-IP]')
+    .option('-d, --destination <path>', 'Destination path for NGINX realip list.', '/etc/nginx/util')
+    .option('-h, --h <header>', 'Header to fetch realip from.', 'CF-Connecting-IP')
     .parse(process.argv);
 
 if (!process.argv.slice(2).length) {
