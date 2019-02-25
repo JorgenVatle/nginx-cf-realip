5355d284323d96c92d3fb7de20731018823ba557
Jørgen Vatle <Jorgen@WcAServices.net>
2019-02-25 06:45:34 +0200

    Correct human-readable header option for CLI.

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import Commander from 'commander';
 
 Commander.version(require('../package.json').version)
     .option('-d, --destination <path>', 'Destination path for NGINX realip list.', '/etc/nginx/util')
-    .option('-h, --h <header>', 'Header to fetch realip from.', 'CF-Connecting-IP')
+    .option('-h, --header <header>', 'Header to fetch realip from.', 'CF-Connecting-IP')
     .parse(process.argv);
 
 if (!process.argv.slice(2).length) {
