6c2b65c53734c235788a9ffb9fb666c2d4682fae
Jørgen Vatle <Jorgen@WcAServices.net>
2019-02-25 07:08:52 +0200

    Build NGINX conf for the provided destination path.

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,29 @@
+import FS from 'fs';
+import Path from 'path';
 import Commander from 'commander';
+import NginxRealip from "./NginxRealip";
 
-Commander.version(require('../package.json').version)
-    .option('-d, --destination <path>', 'Destination path for NGINX realip list.', '/etc/nginx/util')
+const realip = new NginxRealip();
+const cli = Commander.version(require('../package.json').version)
+    .option('-d, --destination <path>', 'Destination path for NGINX realip list.', '/etc/nginx/util/cf-realip.conf')
     .option('-h, --header <header>', 'Header to fetch realip from.', 'CF-Connecting-IP')
     .parse(process.argv);
 
 if (!process.argv.slice(2).length) {
     Commander.help();
 }
+
+realip.buildConfig(cli.header).then((config) => {
+    let base = '.';
+
+    try {
+        if (FS.lstatSync(cli.destination).isDirectory()) {
+            base = 'cf-realip.conf';
+        }
+    } catch {}
+
+    FS.writeFileSync(Path.format({
+        dir: cli.destination,
+        base,
+    }), config);
+});
