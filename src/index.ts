f5824c773e4852acf44a4ea732e7db981659cbb0
Jørgen Vatle <Jorgen@WcAServices.net>
2019-02-25 07:45:49 +0200

    Default destination to current directory.

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,7 @@ import NginxRealip from "./NginxRealip";
 
 const realip = new NginxRealip();
 const cli = Commander.version(require('../package.json').version)
-    .option('-d, --destination <path>', 'Destination path for NGINX realip list.', '/etc/nginx/util/cf-realip.conf')
+    .option('-d, --destination <path>', 'Destination path for NGINX realip list.', '.')
     .option('-h, --header <header>', 'Header to fetch realip from.', 'CF-Connecting-IP')
     .parse(process.argv);
 
