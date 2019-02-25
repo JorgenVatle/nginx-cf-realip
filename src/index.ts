8fb7103acaad17ac6f72deb35ee0ba3db32a66c6
Jørgen Vatle <Jorgen@WcAServices.net>
2019-02-25 06:35:09 +0200

    Add destination and header options to CLI.

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
 import Commander from 'commander';
 
-Commander.version(require('../package.json').version).parse(process.argv);
+Commander.version(require('../package.json').version)
+    .option('-d, --destination <path>', 'Destination path for NGINX realip list. [Default: /etc/nginx/util]')
+    .option('-h, --h <header>', 'Header to fetch realip from. defaults to [Default: CF-Connecting-IP]')
+    .parse(process.argv);
 
 if (!process.argv.slice(2).length) {
     Commander.help();
